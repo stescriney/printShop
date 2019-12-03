89f370ff327d25fa89e9d2e4020641e32f53827f
EC2 Default User <ec2-user@ip-172-31-24-106.ec2.internal>
2019-12-03 23:12:52 +0000

    ready to deploy4

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,10 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+#push to heroku
+config.assets.initialize_on_precompile = false
+
+
 module GolfStore
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
