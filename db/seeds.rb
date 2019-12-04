0b68012a43c6c950ee755e437744ec3efae4b280
stescriney <stephenscriney@gmail.com>
2019-12-04 18:02:08 +0000

    Fixed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,9 @@
 
 
 
-user = User.create!  :email => 'stescriney@gmail.com', :password => 'ste123', :password_confirmation => 'ste123'
+user = User.create!  :email => 'stescriney@hotmail.com', :password => 'ste123', :password_confirmation => 'ste123'
 user.toggle!(:admin)
-user_1 = User.create!  :email => 'custom@google.com', :password => 'password', :password_confirmation => 'password'
+user_1 = User.create!  :email => 'custom1@google.com', :password => 'password', :password_confirmation => 'password'
 
 
 item = Item.create! :title =>"Mona Lisa" , :description =>"Mona Lisa by Da Vinci" , :price =>10.0, :image_url =>"", :category =>"Classical", :brand =>"Print Shop"
