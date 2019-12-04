f08807a3a4e7e3e6275df668a99f83792d1f2eff
EC2 Default User <ec2-user@ip-172-31-86-11.ec2.internal>
2019-12-04 17:52:57 +0000

    commit from EC2

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
 
-# Taken from: https://stackoverflow.com/questions/12418584/seeding-users-with-devise-in-ruby-on-rails
+
+
 user = User.create!  :email => 'stescriney@gmail.com', :password => 'ste123', :password_confirmation => 'ste123'
 user.toggle!(:admin)
 user_1 = User.create!  :email => 'custom@google.com', :password => 'password', :password_confirmation => 'password'
